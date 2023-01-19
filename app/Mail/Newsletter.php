b3767c41beafaefc43e20ec73f20129f1ce18878
gomzyakov <alexander.gomzyakov@gmail.com>
2023-01-19 21:37:31 +0500

    feat: add Mail

---
+++ app/Mail/Newsletter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class Newsletter extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected $posts;
+
+    protected $email;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($posts, $email)
+    {
+        $this->posts = $posts;
+        $this->email = $email;
+    }
+
+    /**
+     * Build the message.
+     */
+    public function build(): Newsletter
+    {
+        return $this->from('hello@app.com', config('app.name', 'Laravel'))
+                    ->subject(__('newsletter.email.subject'))
+                    ->view('emails.newsletter')
+                    ->with([
+                        'posts' => $this->posts,
+                        'email' => $this->email
+                    ]);
+    }
+}
