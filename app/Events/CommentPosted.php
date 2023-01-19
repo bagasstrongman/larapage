4bbb7ec3c93f94281ea7eac457a3f42e0cea51d3
gomzyakov <alexander.gomzyakov@gmail.com>
2023-01-19 21:26:53 +0500

    feat: add CommentPosted event

---
+++ app/Events/CommentPosted.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Events;
+
+use App\Http\Resources\Comment as CommentResource;
+use App\Models\Post;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class CommentPosted implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Comment details
+     *
+     * @var CommentResource
+     */
+    public $comment;
+
+    /**
+     * Post details
+     *
+     * @var Post
+     */
+    private $post;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(CommentResource $comment, Post $post)
+    {
+        $this->comment = $comment;
+        $this->post = $post;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     */
+    public function broadcastOn(): Channel
+    {
+        return new Channel('post.' . $this->post->id);
+    }
+
+    /**
+     * The event's broadcast name.
+     */
+    public function broadcastAs(): string
+    {
+        return 'comment.posted';
+    }
+}
