cd413f6f9e5a8e19f55fab1f69e12eea700a37a7
gomzyakov <alexander.gomzyakov@gmail.com>
2023-01-20 18:09:53 +0500

    feat: add Scopes

---
+++ app/Scopes/PostedScope.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Support\Facades\Auth;
+
+class PostedScope implements Scope
+{
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     */
+    public function apply(Builder $builder, Model $model): void
+    {
+        $user = Auth::user() ?? Auth::guard('api')->user();
+
+        // if not connected or if connected but not admin
+        if (!$user || !$user->isAdmin()) {
+            $builder->where('posted_at', '<=', now());
+        }
+    }
+}
