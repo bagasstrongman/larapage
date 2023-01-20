fb8f4edcd1b32af23b6923c0f1e5a914c066babe
gomzyakov <alexander.gomzyakov@gmail.com>
2023-01-20 18:18:33 +0500

    feat: add `fr` translation

---
+++ lang/fr/dashboard.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'dashboard' => 'Dashboard',
+    'this_week' => 'Cette semaine',
+    'details' => 'Voir en détails',
+    'posts' => 'Articles',
+    'comments' => 'Commentaires',
+    'users' => 'Utilisateurs',
+    'media' => 'Galerie',
+];
